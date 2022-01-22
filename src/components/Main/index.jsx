c962e20cefc4d868cffebf1a19e0325f86cf3585
Iván Bautista <ivanbautistag@gmail.com>
2022-01-21 18:42:44 -0600

    Footer on hover & interactive emoji hover

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useChannelComment } from '../../graphql/custom-hook'
 
 import {
@@ -9,9 +9,32 @@ import {
     StyledInputWrapper,
 } from './styles'
 
+const handleEmojiHover = urlEmoji => {
+    const rootPath =
+        'https://res.cloudinary.com/ivanrice-c/image/upload/v1642795216/discord-clone/fonts/emojis/'
+    const emojis = [
+        '05_Laugh_rzq4mh.png',
+        '10_LMAO_xqkvgt.png',
+        '11_ROFL_qf4jgb.png',
+        '04_Grin_v307u8.png',
+        '08_LOL_zksb6l.png',
+        '07_Really_happy_khzpky.png',
+        '09_Relieved_laughter_i13hnr.png',
+        '01_Smile_ihsfiu.png',
+        '06_Contented_h2b8xa.png',
+    ]
+    let randomEmoji = Math.floor(Math.random() * emojis.length)
+    if (`${rootPath}${emojis[randomEmoji]}` === urlEmoji)
+        randomEmoji = Math.floor(Math.random() * emojis.length)
+
+    return `${rootPath}${emojis[randomEmoji]}`
+}
 const Main = ({ params }) => {
     const { server, channel } = params
     const { data } = useChannelComment(server, channel)
+    const [urlEmoji, setUrlEmoji] = useState(
+        'https://res.cloudinary.com/ivanrice-c/image/upload/v1642795216/discord-clone/fonts/emojis/05_Laugh_rzq4mh.png'
+    )
     if (channel && server && data) {
         console.log('result ', data)
     }
@@ -32,7 +55,7 @@ const Main = ({ params }) => {
                 <h1>{channel}</h1>
             </StyledComment>
             <StyledFooter>
-                <StyledInputWrapper>
+                <StyledInputWrapper urlEmoji={urlEmoji}>
                     <div>
                         <i className="ico-addFile" />
                     </div>
@@ -48,7 +71,15 @@ const Main = ({ params }) => {
                         <i className="ico-gift" />
                         <i className="ico-gif" />
                         <i className="ico-sticker" />
-                        <i className="ico-emoji" />
+                        <i
+                            className="ico-Grin"
+                            onMouseOver={() =>
+                                setUrlEmoji(handleEmojiHover(urlEmoji))
+                            }
+                            onFocus={() =>
+                                setUrlEmoji(handleEmojiHover(urlEmoji))
+                            }
+                        />
                     </div>
                 </StyledInputWrapper>
             </StyledFooter>
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -29,6 +29,11 @@ export const StyledComment = styled.section`
   }
   overflow-y: scroll;
 `
+export const StyledFooter = styled.footer`
+  block-size: 4rem;
+  padding-inline-start: 1rem;
+  padding-inline-end: 1rem;
+`
 export const StyledInputWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -40,44 +45,59 @@ export const StyledInputWrapper = styled.div`
   font-size: 1.7rem;
   inline-size: 3.5;
   overflow: hidden;
-`
-export const StyledFooter = styled.footer`
-  block-size: 4rem;
-  padding-inline-start: 1rem;
-  padding-inline-end: 1rem;
-  & ${StyledInputWrapper} {
-    & div:nth-child(1) {
+
+  & div:nth-child(1) {
+    background-color: transparent;
+    inline-size: 3.5rem;
+    text-align: center;
+    line-height: 0;
+  }
+  & div:nth-child(2) {
+    background-color: transparent;
+    flex: 1;
+    line-height: 0;
+    input[type='text'] {
       background-color: transparent;
-      inline-size: 3.5rem;
-      text-align: center;
-      line-height: 0;
+      color: ${p => p.theme.white};
+      font: ${p => p.theme.body2Regular};
+      vertical-align: center;
+      inline-size: 100%;
+      border: 0;
+      outline: none;
+      &::placeholder {
+        color: ${p => p.theme.grey};
+      }
     }
-    & div:nth-child(2) {
-      background-color: transparent;
-      flex: 1;
-      line-height: 0;
-      input[type='text'] {
-        background-color: transparent;
-        color: ${p => p.theme.white};
-        font: ${p => p.theme.body2Regular};
-        vertical-align: center;
-        inline-size: 100%;
-        border: 0;
-        outline: none;
-        &::placeholder {
-          color: ${p => p.theme.grey};
+  }
+  & div:nth-child(3) {
+    ${p => console.log('url ', p.urlEmoji)}
+    background-color: transparent;
+    box-sizing: border-box;
+    padding-inline-start: 1rem;
+    inline-size: 11rem;
+    text-align: center;
+    line-height: 0;
+    i {
+      padding: 0.35rem;
+      &.ico-Grin {
+        display: inline-block;
+        block-size: 0.77rem;
+        padding-inline-start: 2rem;
+        background-color: red;
+        background: no-repeat url(${p => p.urlEmoji});
+        background-position-x: 0.35rem;
+        filter: grayscale(100%);
+        :hover {
+          filter: grayscale(10%);
         }
       }
     }
-    & div:nth-child(3) {
-      background-color: transparent;
-      box-sizing: border-box;
-      padding-inline-start: 1rem;
-      inline-size: 11rem;
-      text-align: center;
-      line-height: 0;
-      i {
-        padding: 0.35rem;
+  }
+  i {
+    cursor: pointer;
+    :hover {
+      ::before {
+        color: ${p => p.theme.white};
       }
     }
   }
