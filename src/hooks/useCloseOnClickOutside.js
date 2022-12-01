d3f12223d702b050b0985f6734df5a615c226d1e
Iván Bautista <ivanbautistag@gmail.com>
2022-12-01 16:56:37 -0600

    fix: prevent event trigger before render the commponent

--- src/hooks/useCloseOnClickOutside.js
+++ src/hooks/useCloseOnClickOutside.js
@@ -2,16 +2,19 @@ import { useEffect, useRef, useState } from 'react'
 
 export const useCloseOnClickOutside = () => {
   const refClickElement = useRef(null)
+  const refIsFirst = useRef(true)
   const [isOutside, setIsOutside] = useState(false)
   useEffect(() => {
     const handleClickOutside = e => {
-      if (refClickElement.current) {
+      if (refClickElement.current && !refIsFirst.current) {
         const isOutsideElement = !refClickElement.current.contains(e.target)
         if (isOutsideElement) setIsOutside(isOutsideElement)
       }
+      if (refIsFirst.current) refIsFirst.current = false
     }
     const keyDownClose = event => {
-      if (event.keyCode === 27) handleClickOutside(event)
+      if (event.keyCode === 27 && !refIsFirst.current) handleClickOutside(event)
+      if (refIsFirst.current) refIsFirst.current = false
     }
     document.addEventListener('click', handleClickOutside, false)
     document.addEventListener('keydown', keyDownClose, false)
@@ -20,7 +23,7 @@ export const useCloseOnClickOutside = () => {
       document.removeEventListener('click', handleClickOutside)
       document.removeEventListener('keydown', keyDownClose)
     }
-  }, [isOutside])
+  }, [])
 
   return [refClickElement, isOutside]
 }
