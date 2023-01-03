61190ba075fc3fa3d3e7659f588b54a9001ca196
Iván Bautista <ivanbautistag@gmail.com>
2023-01-03 17:02:31 -0600

    New component for helmet titles

---
+++ src/components/TitleMeta/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useSelector } from 'react-redux'
+
+export const TitleMeta = ({ server, channelTitle }) => {
+  const storedServersTitle = useSelector(state => state.app.server) || ''
+  const serverTitle = idServer => storedServersTitle[idServer] || 'No title'
+  let title = '404 Not found'
+  if (server) title = `Discord ${serverTitle(server)}, ${channelTitle}`
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={title} data-react-helmet="true" />
+    </Helmet>
+  )
+}
