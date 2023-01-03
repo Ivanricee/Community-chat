e7247f09ef5b8dfcdf1d25b44f8893a62fdf3396
Iván Bautista <ivanbautistag@gmail.com>
2023-01-03 17:01:26 -0600

    Refactor from children to <div> element

--- src/components/LeftMenu/ItemWrapper.jsx
+++ src/components/LeftMenu/ItemWrapper.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
+import { ImgContainer } from '../ImgContainer'
 
-export const ItemWrapper = ({ className, children }) => {
-  return <div className={className}>{children}</div>
+export const ItemWrapper = ({
+  className,
+  img,
+  alt,
+  notification,
+  borderColor,
+}) => {
+  return (
+    <div className={className}>
+      <ImgContainer
+        img={img}
+        alt={alt}
+        content={notification.content}
+        inlineSize={notification.inlineSize}
+        translateX={notification.translateX}
+        display={notification.display}
+        borderColor={borderColor}
+      />
+    </div>
+  )
 }
