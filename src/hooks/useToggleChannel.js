3ec0db07ae4f7fd4d322fb9072b51a1abeb05157
Iván Bautista <ivanbautistag@gmail.com>
2023-01-09 20:16:52 -0600

    refactor

--- src/hooks/useToggleChannel.js
+++ src/hooks/useToggleChannel.js
@@ -4,8 +4,7 @@ import { setToggleChannel } from '../store/actions/AppActions'
 export const useToggleChannel = () => {
   const dispatch = useDispatch()
   const showChannel = useSelector(state => state.app.toggleChannel)
-  const setShowChannel = option => {
-    return dispatch(setToggleChannel(option))
-  }
+  const setShowChannel = option => dispatch(setToggleChannel(option))
+
   return [showChannel, setShowChannel]
 }
